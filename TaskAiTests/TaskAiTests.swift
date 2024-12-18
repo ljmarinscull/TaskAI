6f7ceb9ee12566fe00ccbc95e3a612dfcf22d222
Lazaro Jesus Marin Scull <ljmarinscull@gmail.com>
2024-12-18 16:20:00 +0200

    Added test to be sure that the client does not make a request on creation

--- TaskAiTests/TaskAiTests.swift
+++ TaskAiTests/TaskAiTests.swift
@@ -5,13 +5,47 @@
 //  Created by Lazaro Jesus Marin Scull on 18.12.24.
 //
 
-import Testing
+import XCTest
 @testable import TaskAi
 
-struct TaskAiTests {
+public enum HTTPClientResult {
+   case success(Data, HTTPURLResponse)
+   case failure(Error)
+}
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
+public protocol HTTPClient {
+   func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
 
+class RemoteApiService: ApiService {
+   let url: URL
+   let client: HTTPClient
+   
+   init (url: URL, client: HTTPClient) {
+      self.url = url
+      self.client = client
+   }
+
+   func request(completion: @escaping (Result<Response, Error>) -> Void){
+      
+   }
+}
+
+class RemoteRequestUseCaseTests: XCTestCase {
+
+   func test_init_doesNotRequestDataFromURL() {
+      let (_, client) = makeSUT()
+
+      XCTAssertTrue(client.requestedURLs.isEmpty)
+   }
+   
+   // MARK: - Helpers
+
+   private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteApiService, client: HTTPClientSpy) {
+      let client = HTTPClientSpy()
+      let sut = RemoteApiService(url: url, client: client)
+      trackForMemoryLeaks(sut, file: file, line: line)
+      trackForMemoryLeaks(client, file: file, line: line)
+      return (sut, client)
+   }
 }
