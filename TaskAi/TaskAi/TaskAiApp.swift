12b1db087660d9db0e8f19bb4da1ebcb30b246a5
Lazaro Jesus Marin Scull <ljmarinscull@gmail.com>
2024-12-18 11:50:33 +0200

    Initial commit

---
+++ TaskAi/TaskAi/TaskAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAiApp.swift
+//  TaskAi
+//
+//  Created by Lazaro Jesus Marin Scull on 18.12.24.
+//
+
+import SwiftUI
+
+@main
+struct TaskAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskAiTests/TaskAiTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAiTests.swift
+//  TaskAiTests
+//
+//  Created by Lazaro Jesus Marin Scull on 18.12.24.
+//
+
+import Testing
+@testable import TaskAi
+
+struct TaskAiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
