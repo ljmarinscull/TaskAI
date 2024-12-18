998f248b090f49e5ae17748fa26e0a549d2ab72b
Lazaro Jesus Marin Scull <ljmarinscull@gmail.com>
2024-12-18 16:42:54 +0200

    Added expect function helper to reuse the logic of comparison of the received and expected response

---
+++ TaskAiTests/Helpers/RemoteRequestUseCaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  RemoteRequestUseCaseTests.swift
+//  TaskAi
+//
+//  Created by Lazaro Jesus Marin Scull on 18.12.24.
+//
+
+import XCTest
+import TaskAi
+
+extension RemoteRequestUseCaseTests {
+   func expect(_ sut: RemoteApiService, toCompleteWith expectedResult: Result<RequestResponse, RemoteApiService.Error>, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+      let exp = expectation(description: "Wait for load completion")
+
+      sut.request { receivedResult in
+         switch (receivedResult, expectedResult) {
+         case let (.success(receivedItems), .success(expectedItems)):
+            XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+
+         case let (.failure(receivedError), .failure(expectedError)):
+            XCTAssertEqual(receivedError as! RemoteApiService.Error, expectedError, file: file, line: line)
+
+         default:
+            XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+         }
+
+         exp.fulfill()
+      }
+
+      action()
+
+      waitForExpectations(timeout: 0.1)
+   }
+}
