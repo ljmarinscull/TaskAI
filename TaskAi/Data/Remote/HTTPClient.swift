a9192fb9f1d05ea4bd77d5d7ca292c467d24a9fa
Lazaro Jesus Marin Scull <ljmarinscull@gmail.com>
2024-12-18 21:26:54 +0200

    Move the URLSessionHTTPClient to a separate file with the HTTPClient

---
+++ TaskAi/Data/Remote/HTTPClient.swift
@@ -0,0 +1,38 @@
+//
+//  HTTPClient.swift
+//  TaskAi
+//
+//  Created by Lazaro Jesus Marin Scull on 18.12.24.
+//
+import Foundation
+
+public enum HTTPClientResult {
+   case success(Data, HTTPURLResponse)
+   case failure(Error)
+}
+
+public protocol HTTPClient {
+   func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
+
+struct UnExpectedValuesRepresentation: Error{}
+class URLSessionHTTPClient: HTTPClient {
+   private let session: URLSession
+   
+   init(session: URLSession = .shared) {
+      self.session = session
+   }
+   
+   func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void){
+      session.dataTask(with: url) { data, response, error in
+         if let error {
+            completion(.failure(error))
+         } else if let data, let response = response as? HTTPURLResponse {
+            completion(.success(data, response))
+         } else {
+            completion(.failure(UnExpectedValuesRepresentation()))
+         }
+      }
+      .resume()
+   }
+}
